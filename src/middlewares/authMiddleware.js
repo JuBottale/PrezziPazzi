a4c0b4b9e20071a1ee4c643118bc797900edf174
JuliBottale <julietabottale@hotmail.com>
2023-07-12 20:25:29 -0300

    agregue popup para authMiddleware

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -2,7 +2,8 @@ function authMiddleware(req,res,next){
     if (req.session.user != undefined){
             next()
     }else{
-        return res.send("Esta pagina es solo para usuarios");
+/*         return res.send("Esta pagina es solo para usuarios"); */
+   return res.render("authMiddleware"); 
     }
 
 };
